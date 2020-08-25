7e0d1ba53139a13748e2d2435a263b7efdd44a66
Mark <bf-109g2@hotmail.com>
2020-08-25 15:07:13 -0400

    Created basic dash app

---
+++ app.py
@@ -0,0 +1,31 @@
+# standard library
+import os
+
+# dash libs
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.figure_factory as ff
+import plotly.graph_objs as go
+
+
+app = dash.Dash()
+app.css.append_css({
+    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
+})
+
+app.layout = html.Div([
+
+    # Page Header
+    html.Div([
+        html.H1('Visualization Dashboard')
+    ]),
+
+])
+
+if __name__ == '__main__':
+    app.run_server(
+        debug=True,
+        host='0.0.0.0',
+        )
