f544a5a1d86beeb8313d26e212de0cce36ecc8d7
Janet Li <janetli@janets-mbp-2.attlocal.net>
2020-08-30 13:44:39 -0400

    minor formatting changes, centered school names

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -49,30 +49,30 @@ import DishaRamchandani from './headshot/disha-ramchandani.png'
 
 const CHAIR = 'Chair'
 const CO_DIRECTOR_LOGISTICS = 'Co-Director of Operations'
-const ASSOC_DIRECTOR_LOGISTICS = 'Assoc. Director Operations'
+const ASSOC_DIRECTOR_LOGISTICS = 'Assc. Director of Operations'
 const CO_DIRECTOR_GROWTH = 'Co-Director of Growth'
-const ASSOC_DIRECTOR_GROWTH = 'Assoc. Director Growth'
+const ASSOC_DIRECTOR_GROWTH = 'Assc. Director of Growth'
 const DIRECTOR_PR = 'Director of PR'
-const ASSOC_DIRECTOR_PR = 'Assoc. Director PR'
+const ASSOC_DIRECTOR_PR = 'Assc. Director of PR'
 const DIRECTOR_BUSINESS = 'Director of Business'
 const CO_DIRECTOR_TECH = 'Co-Director of Tech'
 const DIRECTOR_MEDIA_DESIGN = 'Director of Design'
 const COMMUNITY_COORDINATOR = 'Community Coordinator'
-const SEMINARS_LEAD = 'Seminars Lead'
-const CURRICULAR_LEAD = 'Curricular Lead'
-const SPECIAL_EVENTS_LEAD = 'Special Events Lead'
-const DATA_LEAD = 'Data Lead'
-const OUTREACH_LEAD_WEST = 'Outreach Lead West'
-const OUTREACH_LEAD_MIDWEST = 'Outreach Lead Midwest'
-const OUTREACH_LEAD_SOUTHEAST = 'Outreach Lead Southeast'
-const OUTREACH_LEAD_NORTHEAST = 'Outreach Lead Northeast'
-const OUTREACH_LEAD_INTERNATIONAL = 'Outreach Lead International'
-const SOCIAL_MEDIA_LEAD = 'Social Media Lead'
-const VIDEO_MEDIA_LEAD = 'Video Media Lead'
-const PRESS_CO_LEAD = 'Press Media Co-Lead'
-const SPONSORSHIPS_LEAD = 'Sponsorships Lead'
-const FUNDRAISING_LEAD = 'Fundraising Lead'
-const LEGAL_CO_LEAD = 'Legal Co-Lead'
+const SEMINARS_LEAD = 'Engaged Seminars'
+const CURRICULAR_LEAD = 'Curricular Support'
+const SPECIAL_EVENTS_LEAD = 'Special Events'
+const DATA_LEAD = 'Data'
+const OUTREACH_LEAD_WEST = 'West Outreach'
+const OUTREACH_LEAD_MIDWEST = 'Midwest Outreach'
+const OUTREACH_LEAD_SOUTHEAST = 'Southeast Outreach'
+const OUTREACH_LEAD_NORTHEAST = 'Northeast Outreach'
+const OUTREACH_LEAD_INTERNATIONAL = 'International Outreach'
+const SOCIAL_MEDIA_LEAD = 'Social Media'
+const VIDEO_MEDIA_LEAD = 'Video Media'
+const PRESS_CO_LEAD = 'Press and Written Media'
+const SPONSORSHIPS_LEAD = 'Sponsorships'
+const FUNDRAISING_LEAD = 'Fundraising'
+const LEGAL_CO_LEAD = 'Legal'
 
 const teamData = [
   {
@@ -94,16 +94,16 @@ const teamData = [
     image: KevinTan
   },
   {
-    name: 'Alain Saal-Dalma',
-    school: 'Carnegie Mellon ‘23',
-    position: ASSOC_DIRECTOR_LOGISTICS,
-    image: AlainDalma
-  },
-  {
     name: 'Janet Li',
     school: 'Harvard ‘23',
     position: ASSOC_DIRECTOR_LOGISTICS,
     image: JanetLi
+  },
+  {
+    name: 'Alain Saal-Dalma',
+    school: 'Carnegie Mellon ‘23',
+    position: ASSOC_DIRECTOR_LOGISTICS,
+    image: AlainDalma
   },
   {
     name: 'Joshua Chen',
@@ -130,6 +130,12 @@ const teamData = [
     image: MelinaMuthuswamy
   },
   {
+    name: 'Carter Martindale',
+    school: 'Harvard ‘23',
+    position: DIRECTOR_BUSINESS,
+    image: CarterMartindale
+  },
+  {
     name: 'Nikki Suzani',
     school: 'Gunn HS ‘21',
     position: DIRECTOR_PR,
@@ -142,28 +148,22 @@ const teamData = [
     image: LindaShen
   },
   {
-    name: 'Carter Martindale',
-    school: 'Harvard ‘23',
-    position: DIRECTOR_BUSINESS,
-    image: CarterMartindale
-  },
-  {
     name: 'Steve Li',
     school: 'Harvard ‘23',
     position: CO_DIRECTOR_TECH,
     image: SteveLi
   },
   {
-    name: 'Daniela Shuman',
-    school: 'Harvard ‘23',
-    position: CO_DIRECTOR_TECH,
-    image: DanielaShuman
-  },
-  {
     name: 'Jonny Rogers',
     school: 'Williams College ‘23',
     position: CO_DIRECTOR_TECH,
     image: JonnyRogers
+  },
+  {
+    name: 'Daniela Shuman',
+    school: 'Harvard ‘23',
+    position: CO_DIRECTOR_TECH,
+    image: DanielaShuman
   },
   {
     name: 'Sandra Chiu',
@@ -235,6 +235,18 @@ const leadsData = [
     image: DishaRamchandani
   },
   {
+    name: 'Annie Lin',
+    school: 'UC Berkeley ‘24',
+    position: PRESS_CO_LEAD,
+    image: AnnieLin
+  },
+  {
+    name: 'Patrick Nie',
+    school: 'UMass Amherst ‘22',
+    position: PRESS_CO_LEAD,
+    image: PatrickNie
+  },
+  {
     name: 'Vanessa Yu',
     school: 'University of Toronto ‘24',
     position: SOCIAL_MEDIA_LEAD,
@@ -247,28 +259,16 @@ const leadsData = [
     image: LilyHuynh
   },
   {
-    name: 'Patrick Nie',
-    school: 'UMass Amherst ‘22',
-    position: PRESS_CO_LEAD,
-    image: PatrickNie
-  },
-  {
-    name: 'Annie Lin',
-    school: 'UC Berkeley ‘24',
-    position: PRESS_CO_LEAD,
-    image: AnnieLin
+    name: 'Ruhi Mistry',
+    school: 'UC Santa Cruz ‘24',
+    position: FUNDRAISING_LEAD,
+    image: RuhiMistry
   },
   {
     name: 'Minjue Wu',
     school: 'Harvard ‘22',
     position: SPONSORSHIPS_LEAD,
     image: MinjueWu
-  },
-  {
-    name: 'Ruhi Mistry',
-    school: 'UC Santa Cruz ‘24',
-    position: FUNDRAISING_LEAD,
-    image: RuhiMistry
   },
   {
     name: 'Layth Alkani',
@@ -287,7 +287,8 @@ const leadsData = [
 function getColor (position) {
   switch (position) {
     case CHAIR:
-    case DIRECTOR_BUSINESS:
+    case DIRECTOR_PR:
+    case ASSOC_DIRECTOR_PR:
     case SPONSORSHIPS_LEAD:
     case FUNDRAISING_LEAD:
     case LEGAL_CO_LEAD:
@@ -309,8 +310,7 @@ function getColor (position) {
     case OUTREACH_LEAD_INTERNATIONAL:
     case DIRECTOR_MEDIA_DESIGN:
       return Colors.WLF_YELLOW
-    case DIRECTOR_PR:
-    case ASSOC_DIRECTOR_PR:
+    case DIRECTOR_BUSINESS:
     case SOCIAL_MEDIA_LEAD:
     case VIDEO_MEDIA_LEAD:
     case PRESS_CO_LEAD:
@@ -346,7 +346,7 @@ const Team = () => {
         })}
       </Styles.TeamRow>
       <div>
-          <Typography.Header style={{color: Colors.WLF_ORANGE}}>Team Leads</Typography.Header>
+          <Typography.Header style={{color: Colors.WLF_PURPLE}}>Team Leads</Typography.Header>
       </div>
       <Styles.TeamRow>
         {leadsData.map((person) => {
