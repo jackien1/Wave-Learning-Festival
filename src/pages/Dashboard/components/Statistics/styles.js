ef34883a40e1580da5f8353cc839910818033242
Alan Sun <mralansun@gmail.com>
2020-08-31 17:19:07 -0400

    set up dashboard stats cards (still need css fix)

---
+++ src/pages/Dashboard/components/Statistics/styles.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components'
+import { Colors } from '@/styles'
+
+export const StatsContainer = styled.div `
+    background: none;
+    position: absolute;
+    width: 75%;
+    height: 375px;
+
+    & h1 {
+        font-family: 'Mukta Mahee', sans-serif;
+        font-size: 40px;
+        margin-top: 1em;
+        margin-bottom: 0.5em;
+        color: ${Colors.WLF_PURPLE};
+    }
+
+    & hr {
+        border: 0.5px solid;
+        color: lightgray;
+    }
+
+`
+
+export const CardContainer = styled.div `
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    width: 100%;
+    margin-bottom: 2em;
+
+`
+
+export const StatsCard = styled.div `
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    min-width: 230px;
+    height: 218px;
+    border-radius: 15px;
+    text-align: right;
+    margin-bottom: 1em;
+
+    & img {
+        display: block;
+        margin: auto 1.75em;
+        width: 9em;
+        height: auto;
+    }
+
+    & h1 {
+        position: absolute;
+        font-family: 'Mukta Mahee';
+        font-style: normal;
+        font-weight: 600;
+        font-size: 116px;
+        line-height: 193px;
+        text-shadow: 0 3px 5px ${Colors.DARK_GRAY};
+        color: #ffffff;
+        left: 100px;
+        bottom: 35px;
+        margin: 0;
+        z-index: 1;
+        text-align: right;
+    }
+
+    & p {
+        position: absolute;
+        font-family: 'Muli', sans-serif;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 20px;
+        color: #ffffff;
+        left: 100px;
+        bottom: 30px;
+        margin-right: 2em;
+        text-align: right;
+        z-index: 1;
+    }
+
+`
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -2,34 +2,34 @@ import styled from 'styled-components'
 import { Colors } from '@/styles'
 
 export const Container = styled.div`
-  width: 100%; 
-  height: 100vh; 
-  padding-top: 75px; 
-  display: flex; 
-  z-index: 5; 
+  width: 100%;
+  height: 100vh;
+  padding-top: 75px;
+  display: flex;
+  z-index: 5;
 `
 
 export const Sidebar = styled.div`
-  height: 100vh; 
-  position: fixed; 
-  width: 250px; 
+  height: 100vh;
+  position: fixed;
+  width: 250px;
   background-color: ${Colors.WLF_PURPLE};
   box-shadow: 0 0 1.25rem rgba(31,45,61,0.3);
 `
 
 export const ListItem = styled.p`
-  color: white; 
-  padding-left: 2rem; 
-  font-weight: 700; 
+  color: white;
+  padding-left: 2rem;
+  font-weight: 700;
   padding-top: 1.2rem;
-  padding-bottom: 1.2rem; 
-  margin: 0px; 
-  font-size: 1.1rem; 
-  transition: 0.2s; 
+  padding-bottom: 1.2rem;
+  margin: 0px;
+  font-size: 1.1rem;
+  transition: 0.2s;
 
   &:hover {
-    background-color: rgba(255, 255, 255, 0.2); 
-    cursor: pointer; 
+    background-color: rgba(255, 255, 255, 0.2);
+    cursor: pointer;
   }
 `
 
@@ -38,19 +38,19 @@ export const Highlight = styled.img`
   z-index: 1;
 `
 export const ContentContainer = styled.div`
-  position: relative; 
-  left: 250px;   
-width: calc(100% - 600px); 
-  
-  padding: 2rem; 
-  min-height: 100vh; 
+  position: relative;
+  display: flex;
+  justify-content: center;
+  left: 250px;
+  width: calc(100% - 600px);
+  min-height: 100vh;
 `
 
 export const CalendarContainer = styled.div`
-  position: fixed; 
-  right: 0; 
-  height: 100vh; 
-  width: 350px; 
+  position: fixed;
+  right: 0;
+  height: 100vh;
+  width: 350px;
   box-shadow: -1px 4px 4px rgba(0, 0, 0, 0.25);
-  padding: 2rem; 
+  padding: 2rem;
 `
