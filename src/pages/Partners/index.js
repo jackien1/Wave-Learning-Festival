86986348121bbfc4829f0fb084e6dab425a0447c
Alan Sun <mralansun@gmail.com>
2020-08-31 22:09:57 -0400

    added content to partners page

--- src/pages/Partners/index.js
+++ src/pages/Partners/index.js
@@ -3,6 +3,38 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Container, ContainerInner } from "@/globalStyles";
 import { Colors, Typography } from "@/styles";
+import { PartnerContainer, PartnersRow } from './styles'
+import MEDLIFEOntario from './logos/MEDLIFEOntarioLogo.jpg'
+import SeedsInTheMiddle from './logos/SeedsInTheMiddleLogo.png'
+import Stemey from './logos/STEMEYLogo.png'
+import tinyTouchofLove from './logos/tinyTouchOfLove.png'
+
+const partnerData = [
+  {
+    name: 'MEDLIFE Ontario',
+    logo: MEDLIFEOntario
+  },
+  {
+    name: 'Seeds In The Middle',
+    logo: SeedsInTheMiddle
+  },
+  {
+    name: 'STEMEY',
+    logo: Stemey
+  },
+  {
+    name: 'tiny Touch of Love',
+    logo: tinyTouchofLove
+  }
+]
+
+const partnerIcon = (partner) => {
+  return (
+    <PartnerContainer>
+      <img src={partner.logo}/>
+    </PartnerContainer>
+  )
+}
 
 const Partners = () => {
   return (
@@ -13,6 +45,11 @@ const Partners = () => {
           <Typography.Header style={{ color: Colors.WLF_PURPLE }}>
             Community Partners
           </Typography.Header>
+          <PartnersRow>
+            {partnerData.map((partner) => {
+              return partnerIcon(partner)
+            })}
+          </PartnersRow>
         </ContainerInner>
       </Container>
 
---
+++ src/pages/Partners/styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { device } from "@/theme";
+
+export const PartnerContainer = styled.div`
+  width: 100%;
+  height: auto;
+
+  & img {
+    width: 100%;
+    height: auto;
+  }
+`
+
+export const PartnersRow = styled.div`
+  display: grid;
+  grid-column-gap: 2em;
+  grid-row-gap: 2em;
+  align-items: center;
+  width: 100%;
+
+  @media ${device.mobileL} {
+    padding: 20px 10px;
+    grid-template-columns: repeat(1, [col-start] minmax(200px, 1fr) [col-end]);
+  }
+  @media ${device.tablet} {
+    padding: 20px 10px;
+    grid-template-columns: repeat(2, [col-start] minmax(200px, 1fr) [col-end]);
+  }
+  @media ${device.laptop} {
+    padding: 20px 30px;
+    grid-template-columns: repeat(3, [col-start] minmax(200px, 1fr) [col-end]);
+  }
+  `
