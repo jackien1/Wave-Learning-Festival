7aec45548195886b6ecb743e2a66b03fad7f86e5
Janet Li <janetli@janets-mbp-2.attlocal.net>
2020-09-01 19:50:20 -0400

    updated announcements and added application deadlines

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -164,7 +164,7 @@ const About = () => {
                 Announcements:
                 <Typography.BodyText style={{ color: 'white', fontSize: 16, fontWeight: '100' }}>
                   <ul style={{ marginTop: -10, marginLeft: -25, lineHeight: 1.5 }}>
-                    <li>Instructor applications for Tide 1 are now open! <a href="/instructors" style={{ color: Colors.WLF_YELLOW }}>Learn More</a></li>
+                    <li>Instructor applications for Tide 1 are open until 9/6! <a href="/instructors" style={{ color: Colors.WLF_YELLOW }}>Learn More</a></li>
                     <li>Tutor applications for our fall tutoring program are now open! <a href="/tutors" style={{ color: Colors.WLF_YELLOW }}>Learn More</a></li>
                     <li>Learn how to self-publish a book in a month with Iris Fu on Thursday 9/3! <a href="/speakers" style={{ color: Colors.WLF_YELLOW }}>Learn More</a></li>
                   </ul>
