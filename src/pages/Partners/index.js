7da182f962d7afa913567bd3827764375ba08bec
Alan Sun <mralansun@gmail.com>
2020-09-07 14:22:33 -0400

    added org links to logos; changed header to 'in collaboration with'

--- src/pages/Partners/index.js
+++ src/pages/Partners/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Link } from 'react-router-dom';
 import { Container, ContainerInner } from "@/globalStyles";
 import { Colors, Typography } from "@/styles";
 import { PartnerContainer, PartnersRow } from './styles'
@@ -14,34 +15,41 @@ const partnerData = [
   {
     name: 'MEDLIFE Ontario',
     logo: MEDLIFEOntario,
-    width: 275
+    width: 275,
+    link: 'https://www.medlifeontario.org/'
   },
   {
     name: 'PsychStart',
     logo: PsychStart,
-    width: 200
+    width: 200,
+    link: 'https://www.psychstart.co.uk/'
   },
   {
     name: 'Seeds In The Middle',
     logo: SeedsInTheMiddle,
-    width: 220
+    width: 220,
+    link: 'http://seedsinthemiddle.org/'
   },
   {
     name: 'STEMEY',
     logo: Stemey,
-    width: 225
+    width: 225,
+    link: 'http://www.stemenrichmentyouth.com/'
   },
   {
     name: 'tiny Touch of Love',
     logo: tinyTouchofLove,
-    width: 290
+    width: 290,
+    link: 'http://www.tinytouchoflove.org/'
   }
 ]
 
 const partnerIcon = (partner) => {
   return (
     <PartnerContainer>
-      <img src={partner.logo} width={partner.width}/>
+      <a href={partner.link} target="_blank">
+        <img src={partner.logo} width={partner.width}/>
+      </a>
     </PartnerContainer>
   )
 }
@@ -53,7 +61,7 @@ const Partners = () => {
       <Container>
         <ContainerInner>
           <Typography.Header style={{ color: Colors.WLF_PURPLE }}>
-            Community Partners
+            In Collaboration With
           </Typography.Header>
           <PartnersRow>
             {partnerData.map((partner) => {
