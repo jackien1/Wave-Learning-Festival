d459b749a92e03c8b048cecb12dd182b9c6426af
swathiatgit <sleepyswathi@gmail.com>
2020-09-09 16:15:15 -0700

    Working on seminar info

--- src/pages/Dashboard/TeacherDashboard/index.js
+++ src/pages/Dashboard/TeacherDashboard/index.js
@@ -10,8 +10,9 @@ import WavyPurple from '../../About/assets/wavy_purple.svg'
 import WavyTurquoise from '../../About/assets/wavy_turquoise.svg'
 import Amplify, { API, graphqlOperation } from "aws-amplify"
 import { getTeacher, listTeachers } from "../../../graphql/queries.js"
-import { getSeminar } from "../../../graphql/queries.js"
+import { getSeminar, listSeminar } from "../../../graphql/queries.js"
 import { updateTeacher } from "../../../graphql/mutations.js"
+import { updateSeminar } from "../../../graphql/mutations.js"
 
 
 
@@ -25,14 +26,13 @@ const profileState = {
   country: '',
   city: '',
   // Course Data - refer to AWS GraphQL Schema for types
-  couDescrip: '',
   couTitle:'',
-  couCategory:'', // make a dropdown
   couMaxSize:'',
   couPrereqs:'',
+  couDescrip: '',
+  couCategory:'', // make a dropdown
   couSyllabus:'',
   couTargAud:'',
-  couEnrolled:'',
   // Class-Specific Data
   /* would have each date + 
   Jackie's enrollment data */
@@ -96,6 +96,16 @@ const profileReducer = (state, action) => {
         ...state,
         courseDescription: action.content
       })
+      case 'COUSYLLABUS':
+      return ({
+        ...state,
+        syllabus: action.content
+      })
+      case 'COUTARGAUD':
+      return ({
+        ...state,
+        targetAudience: action.content
+      })
     case 'RESET':
       return (action.content)
   }
@@ -112,11 +122,12 @@ const Dashboard = () => {
   const [localInfo, setLocalInfo] = useState({})
   const [docID, setDocID] = useState('')
   const [profile, profileDispatch] = useReducer(profileReducer, profileState)
-  const [teacher, setTeacher] = useState([])
+  const [teacher, setTeacher] = useState(null)
   const [seminar, setSeminar] = useState(null)
   const [tab, setTab] = useState('course');
   
-  var id = 'student1';
+  var teacherId = 'student1';
+  var seminarId = 'seminar1';
 
   const genFrag = function (label, data, dispatch, state) {
     return {
@@ -139,37 +150,45 @@ const Dashboard = () => {
     ]
   }
 
-  const generateSeminarInfo = function (seminar){
-    var student = API.graphql(graphqlOperation(getTeacher, {id: "student1"}))
-    console.log(student)
+  const generateSeminarInfo = function (seminar) {
     return [
-      // genFrag('CouTitle', seminar.courseTitle, 'COUTITLE', 'title'),
-      // genFrag('CouMaxSize', seminar.maxClassSize, 'COUMAXSIZE', 'maxSize'),
-      // genFrag('CouPreReqs', seminar.prereqs, 'COUPREREQS', 'preReqs'),
-      // genFrag('CouDescrip', seminar.courseDescription, 'COUDESCRIP', 'description')
-
-      genFrag('Tide Title', 'my title!', 'COUTITLE', 'title'),
-      genFrag('Max Size', '20', 'COUMAXSIZE', 'maxSize'),
-      genFrag('Prerequisites', 'just be awesome', 'COUPREREQS', 'preReqs'),
-      genFrag('Description', 'fun fun fun!', 'COUDESCRIP', 'description')
+      genFrag('CouTitle', seminar.data.getSeminar.courseTitle, 'COUTITLE', 'title'),
+      genFrag('CouMaxSize', seminar.data.getSeminar.maxClassSize, 'COUMAXSIZE', 'maxSize'),
+      genFrag('CouPreReqs', seminar.data.getSeminar.prereqs, 'COUPREREQS', 'prereqs'),
+      genFrag('CouDescrip', seminar.data.getSeminar.courseDescription, 'COUDESCRIP', 'description')
     ]
   }
 
  const getTeacherData = async () => {
     try { 
-      const teacherData = await API.graphql(graphqlOperation(getTeacher, {id: "student1"}));      
+      const teacherData = await API.graphql(graphqlOperation(getTeacher, {id: teacherId}));      
     setTeacher(teacherData);
+    console.log(teacherData);
     } catch (error) {
-        console.log('error on fetching songs', error);
+        console.log('error on fetching data', error);
     }
   }
+
+  const getSeminarData = async () => {
+    try { 
+      const seminarData = await API.graphql(graphqlOperation(getSeminar, {id: seminarId}));      
+    setSeminar(seminarData);
+    } catch (error) {
+        console.log('error on fetching data', error);
+    }
+  }
+
 
   useEffect(() => {
     getTeacherData();
   }, []);
 
+  useEffect(() => {
+    getSeminarData();
+  }, []);
 
   useEffect(() => {  }, [wave])
+
 
   useEffect(() => {
     if (teacher) {
@@ -191,15 +210,14 @@ const Dashboard = () => {
 
   useEffect(() => {
     if (seminar) {
-      var courseInfo = generateSeminarInfo(seminar)
-      var courseName = seminar.courseTitle
+      var seminarInfo = generateSeminarInfo(seminar)
       setLocalInfo({
-        title: courseName,
+        title: seminar.courseTitle,
         maxSize: seminar.maxClassSize,
-        preReqs: seminar.prereqs,
+        prereqs: seminar.prereqs,
         description: seminar.courseDescription
       })
-      courseInfo.forEach(label => {
+      seminarInfo.forEach(label => {
         profileDispatch({ type: label.dispatch, content: label.data })
       })
     }
@@ -210,17 +228,25 @@ const Dashboard = () => {
     toggleEdit(false)
   }
 
-  const submit = () => {
+  const submitTeacher = () => {
     console.log(profile);
     API.graphql(graphqlOperation(updateTeacher, {
-      input: {id: id,
+      input: {id: teacherId,
         first_name: profile.firstName,
         last_name: profile.lastName,
         school: profile.school
       }}))
+  }
 
-    setLocalInfo(profile)
-    toggleEdit(false)
+  const submitSeminar = () => {
+    console.log(profile);
+    API.graphql(graphqlOperation(updateSeminar, {
+      input: {id: seminarId,
+        courseTitle: profile.title,
+        maxClassSize: profile.maxSize,
+        prereqs: profile.preReqs,
+        courseDescription: profile.description
+      }}))
   }
 
   if (isError) {
@@ -286,23 +312,33 @@ const Dashboard = () => {
     1 */
   ]
 
-  const openProfile = function (evt, page) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(page).style.display = "block";
-    evt.currentTarget.className += " active";
+  // const openProfile = function (evt, page) {
+  //   var i, tabcontent, tablinks;
+  //   tabcontent = document.getElementsByClassName("tabcontent");
+  //   for (i = 0; i < tabcontent.length; i++) {
+  //     tabcontent[i].style.display = "none";
+  //   }
+  //   tablinks = document.getElementsByClassName("tablinks");
+  //   for (i = 0; i < tablinks.length; i++) {
+  //     tablinks[i].className = tablinks[i].className.replace(" active", "");
+  //   }
+  //   document.getElementById(page).style.display = "block";
+  //   evt.currentTarget.className += " active";
+  // }
+
+  const openTeacherProfilePage = () => {
+    document.getElementById("teacherProfilePage").style.display = "block";
+    document.getElementById("seminarProfilePage").style.display = "none";
+  }
+
+  const openSeminarProfilePage = () => {
+    document.getElementById("teacherProfilePage").style.display = "none";
+    document.getElementById("seminarProfilePage").style.display = "block";
   }
 
   if (teacher) {
-    var teacherInfo = generateTeacherInfo(teacher)
-    var courseInfo = generateSeminarInfo(seminar)
+    var teacherInfo = generateTeacherInfo(teacher);
+    var seminarInfo = generateSeminarInfo(seminar);
     return (<>
       <div>
         <Navbar/>
@@ -319,15 +355,16 @@ const Dashboard = () => {
               <div class = "tab" style = {{
                 paddingBottom: '10px'
               }}>
-                <button className= "tablinks" onClick ={() => setTab('teacher')}>
+                <button className= "toggle-btn" onClick ={() => openTeacherProfilePage()}>
                   <Typography.Header style={{ color: Colors.WLF_PURPLE }}>My Profile</Typography.Header>                
                 </button>
-                <button className = "tablinks" onClick ={() => setTab('course')}>
+                <button className = "toggle-btn" onClick ={() => openSeminarProfilePage()}>
                   <Typography.Header style={{ color: Colors.WLF_TURQOUISE }}>My Tide</Typography.Header>                  
                 </button>            
               </div>
 
-              {tab === 'teacher' && <div id = "TeacherProfile" className = "tabcontent">
+              {/* {tab === 'teacher' && <div id = "TeacherProfile" className = "tabcontent"> */}
+              <div id = "teacherProfilePage">
                 <br></br>
                   <div style={{
                     backgroundImage: `url(${WavyPurple})`,
@@ -357,8 +394,8 @@ const Dashboard = () => {
                       <br/>
                     </Text>
                     {edit && <Row style={{ alignItems: 'center' }}>
-                      <Form.Button onClick={() => submit()} style={{ marginTop: 0, marginRight: 20, marginLeft: 20, width: 100, textAlign: 'center', fontSize: 18 }}>
-                        <b style ={{ color: 'white' }}>Submit</b>
+                      <Form.Button onClick={() => submitTeacher()} style={{ marginTop: 0, marginRight: 20, marginLeft: 20, width: 100, textAlign: 'center', fontSize: 18 }}>
+                        <b style ={{ color: 'white' }}>Submit Changes to Teacher</b>
                       </Form.Button>
                       <Cancel onClick={() => cancel()} style ={{ color: 'white' }}>Cancel</Cancel>
                     </Row>}
@@ -375,12 +412,13 @@ const Dashboard = () => {
                       </Form.Button>
                     </a>
                     <Form.Button onClick={() => toggleEdit(!edit)} style={{ margin: 5, width: 200, textAlign: 'center', fontSize: 18 }}>
-                      <b style ={{ color: 'white' }}>Edit Profile</b>
+                      <b style ={{ color: 'white' }}>Edit Teacher Profile</b>
                     </Form.Button>
                   </Row>
               </div>}
 
-              {tab === 'course' && <div id = "CourseProfile" className = "tabcontent">
+              {/* {tab === 'course' && <div id = "CourseProfile" className = "tabcontent"> */}
+              <div id = "seminarProfilePage">
                   <div style={{
                     backgroundImage: `url(${WavyTurquoise})`,
                     backgroundSize: 'cover',
@@ -390,7 +428,7 @@ const Dashboard = () => {
                   }}>
                     <Text>
                       <br/><br/>
-                      {courseInfo.map((info, index) => {
+                      {seminarInfo.map((info, index) => {
                         return (
                           <Row key={index}>
                             <ProfileLeft>
@@ -409,8 +447,8 @@ const Dashboard = () => {
                       <br/>
                     </Text>
                     {edit && <Row style={{ alignItems: 'center' }}>
-                      <Form.Button onClick={() => submit()} style={{ marginTop: 0, marginRight: 20, marginLeft: 20, width: 100, textAlign: 'center', fontSize: 18 }}>
-                        <b style ={{ color: 'white' }}>Submit</b>
+                      <Form.Button onClick={() => submitSeminar()} style={{ marginTop: 0, marginRight: 20, marginLeft: 20, width: 100, textAlign: 'center', fontSize: 18 }}>
+                        <b style ={{ color: 'white' }}>Submit Changes to Seminar</b>
                       </Form.Button>
                       <Cancel onClick={() => cancel()} style ={{ color: 'white' }}>Cancel</Cancel>
                     </Row>}
@@ -427,7 +465,7 @@ const Dashboard = () => {
                       </Form.Button>
                     </a>
                     <Form.Button onClick={() => toggleEdit(!edit)} style={{ margin: 5, width: 200, textAlign: 'center', fontSize: 18 }}>
-                      <b style ={{ color: 'white' }}>Edit Profile</b>
+                      <b style ={{ color: 'white' }}>Edit Seminar Profile</b>
                     </Form.Button>
                   </Row>
               </div>}
