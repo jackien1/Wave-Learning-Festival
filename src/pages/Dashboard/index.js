29ea9d915f18265a738a42de6d9c3de2b60a84e8
Janet Li <jsli@college.harvard.edu>
2020-09-16 02:20:48 -0400

    IT FINALLY KIND OF WORKS YAY

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -11,6 +11,7 @@ import StudentProfile from './components/StudentProfile'
 import { Auth, API, graphqlOperation } from 'aws-amplify'
 
 import { getStudent } from '../../graphql/queries.js'
+import { updateStudent } from '@/graphql/mutations'
 
 const Dashboard = () => {
   const [email, userEmail] = useState('')
@@ -23,54 +24,98 @@ const Dashboard = () => {
     }
   }
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  // THINGS TO DUPLICATE FOR THE SEMINARS
+
   const profileState = {
-    // Personal Data
-    firstName: '',
-    lastName: '',
+    first_name: '',
+    last_name: '',
     email: '',
+    parent_first_name: '',
+    parent_last_name: '',
+    parentEmail: '',
+    grade: '',
     school: '',
     country: '',
-    city: ''
+    state: '',
+    city: '',
+    howYouHear: '',
+    orgs: []
   }
 
-  const profileReducer = (state, action) => {
+  const profileReducer = (currstate, action) => {
     switch (action.type) {
       case 'FIRSTNAME':
         return ({
-          ...state,
-          firstName: action.content
+          ...currstate,
+          first_name: action.content
         })
       case 'LASTNAME':
         return ({
-          ...state,
-          lastName: action.content
+          ...currstate,
+          last_name: action.content
         })
       case 'EMAIL':
         return ({
-          ...state,
+          ...currstate,
           email: action.content
+        })
+        case 'PARENTFIRSTNAME':
+        return ({
+          ...currstate,
+          parent_first_name: action.content
+        })
+      case 'PARENTLASTNAME':
+        return ({
+          ...currstate,
+          parent_last_name: action.content
+        })
+      case 'PARENTEMAIL':
+        return ({
+          ...currstate,
+          parentEmail: action.content
+        })
+      case 'GRADE':
+        return ({
+          ...currstate,
+          grade: action.content
         })
       case 'SCHOOL':
         return ({
-          ...state,
+          ...currstate,
           school: action.content
         })
       case 'COUNTRY':
         return ({
-          ...state,
+          ...currstate,
           country: action.content
+        })
+      case 'STATE':
+        return ({
+          ...currstate,
+          state: action.content
         })
       case 'CITY':
         return ({
-          ...state,
+          ...currstate,
           city: action.content
+        })
+      case 'HOWYOUHEAR':
+        return ({
+          ...currstate,
+          howYouHear: action.content
+        })
+      case 'ORGS':
+        return ({
+          ...currstate,
+          orgs: action.content
         })
       case 'RESET':
         return (action.content)
     }
   }
 
-    const [tab, setTab] = useState(true)
+    const [page, setPage] = useState(true)
     const [loading, setLoading] = useState(true)
     const [isError, setError] = useState(false)
     const [user, setUser] = useState(null)
@@ -91,12 +136,19 @@ const Dashboard = () => {
 
     const generateStudentInfo = function (student) {
       return [
-        genFrag('First Name', student.data.getStudent.first_name, 'FIRSTNAME', 'firstName'),
-        genFrag('Last Name', student.data.getStudent.last_name, 'LASTNAME', 'lastName'),
-        genFrag('Email', student.data.getStudent.email, 'EMAIL', 'email'),
+        genFrag('Student First Name', student.data.getStudent.first_name, 'FIRSTNAME', 'first_name'),
+        genFrag('Student Last Name', student.data.getStudent.last_name, 'LASTNAME', 'last_name'),
+        genFrag('Student Email', student.data.getStudent.email, 'EMAIL', 'email'),
+        genFrag('Parent First Name', student.data.getStudent.parent_first_name, 'PARENTFIRSTNAME', 'parent_first_name'),
+        genFrag('Parent Last Name', student.data.getStudent.parent_last_name, 'PARENTLASTNAME', 'parent_last_name'),
+        genFrag('Parent Email', student.data.getStudent.parentEmail, 'PARENTEMAIL', 'parentEmail'),
+        genFrag('Grade Level', student.data.getStudent.grade, 'GRADE', 'grade'),
         genFrag('School', student.data.getStudent.school, 'SCHOOL', 'school'),
         genFrag('Country', student.data.getStudent.country, 'COUNTRY', 'country'),
-        genFrag('City', student.data.getStudent.city, 'CITY', 'city')
+        genFrag('State', student.data.getStudent.state, 'COUNTRY', 'state'),
+        genFrag('City', student.data.getStudent.city, 'CITY', 'city'),
+        genFrag('How You Hear', student.data.getStudent.howYouHear, 'HOWYOUHEAR', 'howYouHear'),
+        genFrag('Orgs', student.data.getStudent.orgs, 'ORGS', 'orgs')
       ]
     }
 
@@ -123,12 +175,19 @@ const Dashboard = () => {
       if (student) {
         var studentInfo = generateStudentInfo(student)
         setLocalStudentInfo({
-          firstName: student.data.getStudent.first_name,
-          lastName: student.data.getStudent.last_name,
+          first_name: student.data.getStudent.first_name,
+          last_name: student.data.getStudent.last_name,
           email: student.data.getStudent.email,
+          parent_first_name: student.data.getStudent.parent_first_name,
+          parent_last_name: student.data.getStudent.parent_last_name,
+          parentEmail: student.data.getStudent.parentEmail,
+          grade: student.data.getStudent.grade,
           school: student.data.getStudent.school,
           country: student.data.getStudent.country,
-          city: student.data.getStudent.city
+          state: student.data.getStudent.state,
+          city: student.data.getStudent.city,
+          howYouHear: student.data.getStudent.howYouHear,
+          orgs: student.data.getStudent.orgs
         })
         studentInfo.forEach(label => {
           profileDispatch({ type: label.dispatch, content: label.data })
@@ -136,13 +195,40 @@ const Dashboard = () => {
       }
     }, [student])
 
+    const cancelProfile = () => {
+      profileDispatch({ type: 'RESET', content: localStudentInfo })
+    }
 
-  useEffect(() => {
-    Auth.currentAuthenticatedUser({
-      bypassCache: false // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-    }).then(user => userEmail(user.attributes.email))
-      .catch(err => window.location.href = '/')
-  }, [])
+    const submitProfile = () => {
+      API.graphql(graphqlOperation(updateStudent, {
+        input: {
+          id: studentId,
+          city: profile.city,
+          state: profile.state,
+          country: profile.country,
+          school: profile.school,
+          first_name: profile.first_name,
+          last_name: profile.last_name,
+          email: profile.email,
+          grade: profile.grade,
+          howYouHear: profile.howYouHear,
+          parent_first_name: profile.parent_first_name,
+          parent_last_name: profile.parent_last_name,
+          parentEmail: profile.parentEmail,
+          orgs: profile.orgs
+        }
+      }))
+      setLocalStudentInfo(profile)
+      toggleEdit(false)
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    useEffect(() => {
+      Auth.currentAuthenticatedUser({
+        bypassCache: false // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+      }).then(user => userEmail(user.attributes.email))
+        .catch(err => window.location.href = '/')
+    }, [])
 
     return (<>
       {/* <Navbar user={email}/>
@@ -178,7 +264,7 @@ const Dashboard = () => {
           <ListItem>
             Curricular Support
           </ListItem>
-          <ListItem onClick={() => '/dashboard/student-profile'}>
+          <ListItem onClick={() => setPage(true)}>
             Profile
           </ListItem>
           <ListItem onClick={() => signOut()}>
@@ -195,7 +281,24 @@ const Dashboard = () => {
           </ListItem>
         </Sidebar>
         <ContentContainer>
-          <StatsCards/>
+          {/* <StatsCards/> */}
+          {page && <StudentProfile 
+          first_name = {profile.first_name}
+          last_name = {profile.last_name}
+          email = {profile.email}
+          parent_first_name = {profile.parent_first_name}
+          parent_last_name = {profile.parent_last_name}
+          parentEmail = {profile.parentEmail}
+          grade = {profile.grade}
+          school = {profile.school}
+          country = {profile.country}
+          state = {profile.state}
+          city = {profile.city}
+          howYouHear = {profile.howYouHear}
+          orgs = {profile.orgs}
+          profileDispatch = {profileDispatch}
+          cancelProfile = {cancelProfile}
+          submitProfile = {submitProfile}  />}
         </ContentContainer>
         <CalendarContainer>
           <Calendar />
