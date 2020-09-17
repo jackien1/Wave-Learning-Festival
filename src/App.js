37c444afd3882cbf40cc44fda83c5f26951ca492
Janet Li <jsli@college.harvard.edu>
2020-09-17 04:04:25 -0400

    hid courses page

--- src/App.js
+++ src/App.js
@@ -126,9 +126,9 @@ function App () {
             <Route path="/faq-tutors">
               <FAQTutors />
             </Route>
-            <Route path="/courses">
+            {/* <Route path="/courses">
               <Courses />
-            </Route>
+            </Route> */}
             <Route path="/courses-inprogress">
               <CoursesInProgress />
             </Route>
